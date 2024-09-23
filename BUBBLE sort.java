397451d826d6eb5857131865f8e1a6d978595a10
Karunaselvaraj <159015527+Karunaselvaraj@users.noreply.github.com>
2024-09-23 11:44:29 +0530

    BUBBLE sort.java

---
+++ BUBBLE sort.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class bubblesort {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int [] arr={9,2,7,3,6,2};
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+    }
+}
